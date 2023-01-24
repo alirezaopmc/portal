dad1766fe829e8e47b7d26c7c567d42b627039ad
AlirezaOpmc <alirezaopmc@gmail.com>
2023-01-24 08:57:34 +0330

    add all

--- src/services/minecraft/setup.sh
+++ src/services/minecraft/setup.sh
@@ -4,9 +4,7 @@ docker compose up -d
 container=$(docker ps --format "{{.Names}}" | grep minecraft)
 is_running_cmd="docker container inspect "$container" -f '{{.State.Status}}'"
 
-echo $(eval $is_running_cmd)
-
-until eval $is_running_cmd
+until -z $(eval $is_running_cmd)
 do
     sleep 1
 done
@@ -14,5 +12,5 @@ done
 path="$(pwd)/$(dirname -- "$0")"
 
 mkdir "$path/minecraft-data/scripts"
-cp -r "$path/scripts/*" "$path/minecraft-data/scripts"
+cp -r "$path/scripts/" "$path/minecraft-data/"
 docker exec $container bash /data/scripts/init.sh
